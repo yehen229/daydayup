111717450642e5ac26c7f3a6c4a6e2f7d526e385
yehen229 <1625569388@qq.com>
2022-09-12 15:46:21 +0800

    test

--- curry_原理.js
+++ curry_原理.js
@@ -5,7 +5,6 @@ function getSum(a, b, c) {
     return a + b + c
 }
 
-const curried = _.curry(getSum)
 
 console.log(curried(1,2,3));
 console.log(curried(1,2)(3));
@@ -21,3 +20,6 @@ function curry(func) {
         return func(...args)
     }
 }
+
+const curried = curry(getSum)
+
